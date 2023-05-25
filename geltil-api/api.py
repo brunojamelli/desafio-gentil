839758fa499197976440b984df9c939748d255e9
brunojamelli <brunojamelli@ufrn.edu.br>
2023-05-24 23:55:40 -0300

    refatoração código python api

--- geltil-api/api.py
+++ geltil-api/api.py
@@ -16,7 +16,6 @@ def db_connect():
 # Rota para criar um novo registro na tabela Pessoas
 @app.route('/pessoas', methods=['POST'])
 def criar_pessoa():
-    #cur = conn.cursor()
     conn, cur = db_connect();
     # Recuperar os dados do JSON enviado na requisição
     data = request.get_json()
@@ -48,37 +47,46 @@ def criar_pessoa():
     conn.close()
 
     return "Registro criado com sucesso"
-# Rota para obter todos os registros da tabela Pessoas
 
+# Rota para atualizar registros na tabela de Pessoas
+@app.route('/pessoas', methods=['PUT'])
+def atualizar_pessoa():
+    data = request.json
+    id_pessoa = data['idPessoa']
+    objetivo = data['objetivo']
+    nome = data['nome']
+    data_nascimento = data['dataNascimento']
+    salario = data['salario']
+    observacoes = data['observacoes']
+    nome_mae = data['nomeMae']
+    nome_pai = data['nomePai']
+    cpf = data['cpf']
 
-@app.route('/pessoas', methods=['GET'])
-def get_pessoas():
     conn, cur = db_connect()
-    #cur = conn.cursor()
 
-    cur.execute("SELECT * FROM Pessoas")
-    rows = cur.fetchall()
+    try:
+        cur = conn.cursor()
 
-    # Converter os resultados em um formato JSON
-    pessoas = []
-    for row in rows:
-        pessoa = {
-            'idPessoa': row[0],
-            'objetivo': row[1],
-            'nome': row[2],
-            'dataNascimento': str(row[3]),
-            'salario': float(row[4]),
-            'observacoes': row[5],
-            'cpf': row[6],
-            'nomeMae': row[7],
-            'nomePai': row[8]
-        }
-        pessoas.append(pessoa)
+        # Chamar a procedure de atualização de pessoa
+        cur.execute("CALL atualizar_pessoa(%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                    (id_pessoa, objetivo, nome, data_nascimento, salario, observacoes, nome_mae, nome_pai, cpf))
 
-    cur.close()
-    conn.close()
+        # Recuperar o texto retornado pela procedure
+        resultado = cur.fetchone()
 
-    return jsonify(pessoas)
+        # Imprimir o texto no console
+        print(resultado)
+
+        cur.close()
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        conn.close()
+
+    return 'Registro atualizado com sucesso'
 
 @app.route('/pessoas/<int:id>', methods=['DELETE'])
 def remover_pessoa(id):
@@ -109,6 +117,64 @@ def remover_pessoa(id):
 
     finally:
         conn.close()
+
+# Rota para obter todos os registros da tabela Pessoas
+@app.route('/pessoas2', methods=['GET'])
+def get_pessoas():
+    conn, cur = db_connect()
+    #cur = conn.cursor()
+
+    cur.execute("SELECT * FROM Pessoas")
+    rows = cur.fetchall()
+
+    # Converter os resultados em um formato JSON
+    pessoas = []
+    for row in rows:
+        pessoa = {
+            'idPessoa': row[0],
+            'objetivo': row[1],
+            'nome': row[2],
+            'dataNascimento': str(row[3]),
+            'salario': float(row[4]),
+            'observacoes': row[5],
+            'cpf': row[6],
+            'nomeMae': row[7],
+            'nomePai': row[8]
+        }
+        pessoas.append(pessoa)
+
+    cur.close()
+    conn.close()
+
+    return jsonify(pessoas)
+
+@app.route('/pessoas', methods=['GET'])
+def selecionar_pessoas():
+    conn, cur = db_connect()
+
+    try:
+        cur = conn.cursor()
+
+        # Chamar a procedure de seleção de pessoas
+        cur.execute("CALL selecionar_todas_pessoas()")
+
+        # Recuperar os dados retornados pela procedure
+        resultado = cur.fetchall()
+
+        # Imprimir os dados no console
+        for row in resultado:
+            print(row)
+
+        cur.close()
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        conn.close()
+
+    return 'Registros selecionados com sucesso'
 
 # Rota para selecionar um registro da tabela Pessoas
 @app.route('/pessoas/<int:id>', methods=['GET'])
